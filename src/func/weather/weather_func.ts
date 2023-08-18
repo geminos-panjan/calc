80721cb97dfd730bb23286b3c224aa77075f66e8
geminos-panjan <98257842+geminos-panjan@users.noreply.github.com>
2023-08-18 22:33:34 +0900

    add temperature function

--- src/func/weather/weather_func.ts
+++ src/func/weather/weather_func.ts
@@ -1,8 +1,8 @@
 import { InvalidArgsError } from "../../error.js";
 import { CalcFunction, parseNum, parseString } from "../calc_func.js";
-import { TempUnit, convertTemperature } from "./temperature.js";
+import { TempUnit, ZERO_K, convertTemperature } from "./temperature.js";
 
-export type WeatherFunctionKey = "temper";
+export type WeatherFunctionKey = "temper" | "C2F" | "C2K" | "F2C" | "K2C";
 
 export const weatherFuncs: { [key in WeatherFunctionKey]: CalcFunction } = {
   temper: {
@@ -40,5 +40,29 @@ export const weatherFuncs: { [key in WeatherFunctionKey]: CalcFunction } = {
       '2. temper(n, "from", "to") nを温度単位fromから温度単位toに変換',
       "温度単位はC(摂氏), F(華氏), K(絶対温度)に対応",
     ],
+  },
+  C2F: {
+    funcs: {
+      1: (n) => parseNum(n) * 1.8 + 32,
+    },
+    description: ["C2F(n) °C → °F"],
+  },
+  C2K: {
+    funcs: {
+      1: (n) => parseNum(n) - ZERO_K,
+    },
+    description: ["C2K(n) °C → K"],
+  },
+  F2C: {
+    funcs: {
+      1: (n) => (parseNum(n) - 32) / 1.8,
+    },
+    description: ["F2C(n) °F → °C"],
+  },
+  K2C: {
+    funcs: {
+      1: (n) => parseNum(n) + ZERO_K,
+    },
+    description: ["K2C(n) K → °C"],
   },
 };
